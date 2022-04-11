ef9ddbbffc07c05c91f10099f2506658fddabff7
nlndhlm <30705699+nlndhlm@users.noreply.github.com>
2022-04-11 11:10:52 +0200

    Add files via upload

---
+++ selenium-understat.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+from selenium import webdriver
+import time
+import pandas as pd
+
+# datainnhenting
+ 
+driver = webdriver.Firefox()
+driver.get("https://understat.com/league/EPL")
+ 
+html = driver.page_source
+time.sleep(2)
+
+driver.quit()
+
+#####
+
+data = pd.read_html(html)
+
+teams = data[0]     # lagdata
+players = data[1]   # spillerdata
+
+players["xG"].str.split("+", expand=False)
+
+print(players[["Player", "xG"]])
